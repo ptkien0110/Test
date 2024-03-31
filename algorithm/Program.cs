206121bd8ba01b62e9d18c2f49c2ffa1d34b7d32
ptkien0110 <ptkien0110@gmail.com>
2024-03-31 15:49:13 +0700

    Commit

---
+++ algorithm/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Solution
+{
+    static void miniMaxSum(int[] arr)
+    {
+        long minSum = long.MaxValue;
+        long maxSum = long.MinValue;
+        long totalSum = 0;
+
+        foreach (int num in arr)
+        {
+            totalSum += num;
+            minSum = Math.Min(minSum, num);
+            maxSum = Math.Max(maxSum, num);
+        }
+
+        long minSumResult = totalSum - maxSum;
+        long maxSumResult = totalSum - minSum;
+
+        Console.WriteLine($"{minSumResult} {maxSumResult}");
+    }
+
+    static void Main(string[] args)
+    {
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        miniMaxSum(arr);
+    }
+}
